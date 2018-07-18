f712a9857de8ebc6690b34763ee56781dea9d341
Stanislav Nikles <niklstanislav2@gmail.com>
2018-07-18 11:05:26 +0300

    Initial

---
+++ src/main/java/com/github/stanislavnikles/onlinelibrary/OnlineLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.github.stanislavnikles.onlinelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibraryApplication.class, args);
+    }
+}
