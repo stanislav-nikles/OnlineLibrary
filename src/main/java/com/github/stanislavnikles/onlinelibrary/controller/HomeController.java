e29b47595ec822a57a1454b98c1b304abe766b8b
Stanislav Nikles <niklstanislav2@gmail.com>
2018-07-21 20:40:30 +0300

    Fix HomeController

--- src/main/java/com/github/stanislavnikles/onlinelibrary/controller/HomeController.java
+++ src/main/java/com/github/stanislavnikles/onlinelibrary/controller/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+public class HomeController {
 
-    @RequestMapping(value = {"/", "/main"})
+    @RequestMapping(value = {"/", "/home"})
     public String getMainPage(Model model) {
         model.addAttribute("message", "Online Library!");
-        return "main";
+        return "home";
     }
 }
